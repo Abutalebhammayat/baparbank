9255e5ce68a083cebed617a77bc3764103aa4cd1
Abutalebhammayat <iamaabutaleb461@gmail.com>
2023-09-09 17:25:26 +0600

    three commit

--- styles/log.js
+++ styles/log.js
@@ -8,7 +8,7 @@ document.getElementById('btn-fild').addEventListener('click', function(){
     const password = passwordFiled.value ;
     // stap-4s
     if(email === 'kaziabutaleb.hammayat@gmail.com' &&  password === 'hamayat77'){
-        window.location.href ='http://127.0.0.1:5500/new.html';
+        window.location.href ='https://abutalebhammayat.github.io/baparbank/new.html';
     }
     else{
         alert('Abutaleb boss is it you? I think you do it, Sorry Sorry Sorry 🙏🙏🙏🙏 password show again. ')
